91a3da02748833752ebf96415681f137e94acfa6
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2017-09-21 12:24:20 +0100

    Update broken link in footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,7 @@
   <footer class="footer is-light">
     <div class="container">
       <div class="content has-text-centered">
-        The <strong xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">Syntacticus</strong> software was written by <a xmlns:cc="http://creativecommons.org/ns#" href="http://johndal.com" property="cc:attributionName" rel="cc:attributionURL">Marius L. Jøhndal</a> and is licensed under the <a href="https://opensource.org/licenses/mit-license.php">MIT</a> license. The treebank content is copyrighted by the <router-link to='/about#contributors'>contributors</router-link> to the treebank and licensed under the <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a> license.
+        The <strong xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">Syntacticus</strong> software was written by <a xmlns:cc="http://creativecommons.org/ns#" href="http://johndal.com" property="cc:attributionName" rel="cc:attributionURL">Marius L. Jøhndal</a> and is licensed under the <a href="https://opensource.org/licenses/mit-license.php">MIT</a> license. The treebank content is copyrighted by the <router-link to='/about#licensing'>contributors</router-link> to the treebank and licensed under the <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a> license.
       </div>
     </div>
   </footer>
