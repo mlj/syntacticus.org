b36abc746086385cf1b04fd62af94618ef146c25
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2017-11-21 21:01:32 +0000

    Adapt navbar to Bulma 0.6.1

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,52 +2,53 @@
   <section class="hero is-medium"> <!-- v-shortkey.once="['h']" @shortkey="openModal()" -->
     <div class="hero-head">
       <div class="container">
-        <nav class="nav">
-          <div class="nav-left">
-            <router-link to="/" class="nav-item" exact><img src="../assets/logo.svg" alt="Syntacticus"> SYNTACTICUS</router-link>
-          </div>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+          <div class="navbar-brand">
+            <router-link to="/" class="navbar-item" exact>
+              <img src="../assets/logo.svg" alt="Syntacticus: A treebank of early Indo-European languages" height="32" width="32"> SYNTACTICUS
+            </router-link>
 
-          <!-- Shared menu -->
-          <div class="nav-center">
-            <!-- a class="nav-item" href="#"><span class="icon"><i class="fa fa-github"></i></span></a -->
-            <!-- a class="nav-item" href="#"><span class="icon"><i class="fa fa-twitter"></i></span></a -->
+            <!-- Mobile menu -->
+            <button class="navbar-burger" @click.prevent="toggleNav" v-bind:class="{ 'is-active': navToggled }">
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
           </div>
-
-          <!-- Mobile menu -->
-          <span class="nav-toggle" @click.prevent="toggleNav" v-bind:class="{ 'is-active': navToggled }">
-            <span></span>
-            <span></span>
-            <span></span>
-          </span>
 
           <!-- Desktop menu -->
-          <div class="nav-right nav-menu" v-bind:class="{ 'is-active': navToggled }">
-            <!-- a class="nav-item" href="http://johndal.com/blog/syntacticus">News</a -->
-            <router-link to="/browse" class="nav-item">Browse</router-link>
-            <router-link to="/about" class="nav-item">About</router-link>
+          <div class="navbar-menu" v-bind:class="{ 'is-active': navToggled }">
+            <div class="navbar-start">
+            </div>
 
-            <span class="nav-item">
-              <div class="field has-addons">
-                <p class="control">
-                  <input @keyup.enter="search" v-model="query" class="input" type="text" placeholder=""> <!-- v-shortkey.focus="['s']" -->
-                </p>
-                <p class="control">
-                  <button @click.prevent="search" class="button">Search</button>
-                </p>
+            <div class="navbar-end">
+              <!-- a class="navbar-item" href="http://johndal.com/blog/syntacticus">News</a -->
+              <router-link to="/browse" class="navbar-item">Browse</router-link>
+              <router-link to="/about" class="navbar-item">About</router-link>
+
+              <div class="navbar-item">
+                <div class="field has-addons">
+                  <p class="control">
+                    <input @keyup.enter="search" v-model="query" class="input" type="text" placeholder=""> <!-- v-shortkey.focus="['s']" -->
+                  </p>
+                  <p class="control">
+                    <button @click.prevent="search" class="button">Search</button>
+                  </p>
+                </div>
               </div>
-            </span>
 
-            <span class="nav-item">
-              <!-- a class="button">
-                <span class="icon"><i class="fa fa-twitter"></i></span>
-                <span>Tweet</span>
-              </a -->
+              <!--span class="navbar-item">
+                <a class="button">
+                  <span class="icon"><i class="fa fa-twitter"></i></span>
+                  <span>Tweet</span>
+                </a>
 
-              <!-- a class="button is-primary" href="https://github.com/proiel/proiel-treebank/releases" target="_blank">
-                <span class="icon"><i class="fa fa-download"></i></span>
-                <span>Download</span>
-              </a -->
-            </span>
+                <a class="button is-primary" href="https://github.com/proiel/proiel-treebank/releases" target="_blank">
+                  <span class="icon"><i class="fa fa-download"></i></span>
+                  <span>Download</span>
+                </a>
+              </span -->
+            </div>
           </div>
         </nav>
       </div>
