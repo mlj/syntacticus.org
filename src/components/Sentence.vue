7aa8be7fc6d6af6a0c079c91473bd04ba3e7aab7
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2018-02-21 16:57:13 +0000

    Add CC icons

--- src/components/Sentence.vue
+++ src/components/Sentence.vue
@@ -16,6 +16,7 @@
           <div class="column is-narrow has-text-right">
             <a :href="treebank.mainURL">{{ treebank.name }}</a> version {{ treebank.version }}
             <br>
+            <span class="icon"><i class="fa fa-creative-commons"></i></span>
             <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a> license
             <br>
             {{ sentence.citation }}
