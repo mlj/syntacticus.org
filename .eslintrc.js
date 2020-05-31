5c6986c9e2635c063c1be8c65f79c9ba01ceeec7
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2020-05-31 14:40:41 +0100

    Some reordering to reduce diffs when adding new dependencies using @vue/cli

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,20 @@
 module.exports = {
   root: true,
   parser: 'babel-eslint',
+
   parserOptions: {
     sourceType: 'module'
   },
+
   env: {
     browser: true,
   },
+
   // required to lint *.vue files
   plugins: [
     'html'
   ],
+
   // add your custom rules here
   'rules': {
     // allow debugger during development
