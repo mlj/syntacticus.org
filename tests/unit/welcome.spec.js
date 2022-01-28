42baa9ca574e679d2e6c26c431cf5ee38a64e6ae
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2022-01-28 18:45:42 +0000

    Add jest unit testing

---
+++ tests/unit/welcome.spec.js
@@ -0,0 +1,14 @@
+import { RouterLinkStub } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
+import Welcome from '@/components/Welcome'
+
+describe('Welcome.vue', () => {
+  it('renders the introductory text', () => {
+    const wrapper = shallowMount(Welcome, {
+      propsData: { },
+      stubs: { RouterLink: RouterLinkStub }
+    })
+    expect(wrapper.text()).toMatch('Rigorous')
+    expect(wrapper.text()).toMatch('Comprehensive')
+  })
+})
