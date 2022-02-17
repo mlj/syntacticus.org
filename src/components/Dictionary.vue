f2270eb7a90ab2bd3e387b476a84afe4fb0a5f54
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2022-02-17 16:27:53 +0000

    Factor out a PartOfSpeechSelect component

--- src/components/Dictionary.vue
+++ src/components/Dictionary.vue
@@ -38,53 +38,7 @@
             <input class="input" v-model="formPattern" placeholder="Form">
           </div>
 
-          <b-select v-model="pos">
-            <option :value="null">Any part of speech</option>
-
-            <option value="V-">Verbs</option>
-
-            <optgroup label="Nouns">
-              <option value="Nb">Common nouns</option>
-              <option value="Ne">Proper nouns</option>
-            </optgroup>
-
-            <option value="A-">Adjectives</option>
-
-            <optgroup label="Adverbs">
-              <option value="Df">Adverbs</option>
-              <option value="Dq">Relative adverbs</option>
-              <option value="Du">Interrogative adverbs</option>
-            </optgroup>
-
-            <optgroup label="Pronouns">
-              <option value="Pc">Reciprocal pronouns</option>
-              <option value="Pd">Demonstrative pronouns</option>
-              <option value="Pi">Interrogative pronouns</option>
-              <option value="Pk">Personal reflexive pronouns</option>
-              <option value="Pp">Personal pronouns</option>
-              <option value="Pr">Relative pronouns</option>
-              <option value="Ps">Possessive pronouns</option>
-              <option value="Pt">Possessive reflexive pronouns</option>
-              <option value="Px">Indefinite pronouns</option>
-              <option value="Py">Quantifiers</option>
-            </optgroup>
-
-            <optgroup label="Numerals">
-              <option value="Ma">Cardinal numerals</option>
-              <option value="Mo">Ordinal numerals</option>
-            </optgroup>
-
-            <option value="S-">Articles</option>
-            <option value="R-">Prepositions</option>
-            <option value="N-">Infinitive markers</option>
-            <option value="C-">Conjunctions</option>
-            <option value="G-">Subjunctions</option>
-            <option value="I-">Interjections</option>
-
-            <option value="F-">Foreign words</option>
-
-            <option value="X-">Unassigned words</option>
-          </b-select>
+          <part-of-speech-select v-model="pos" />
 
           <div class="control" v-if="language === 'orv'">
             English translations
@@ -141,6 +95,7 @@
 <script>
 import { mapGetters } from 'vuex';
 import MetadataModal from './MetadataModal';
+import PartOfSpeechSelect from './PartOfSpeechSelect'
 import api from '../api';
 import schema from '../data/schema.json';
 import {
@@ -153,6 +108,7 @@ import {
 export default {
   components: {
     MetadataModal,
+    PartOfSpeechSelect,
   },
 
   props: ['gid'],
---
+++ src/components/PartOfSpeechSelect.vue
@@ -0,0 +1,79 @@
+<template>
+  <b-select v-model="computedValue">
+    <option :value="null">Any part of speech</option>
+
+    <option value="V-">Verbs</option>
+
+    <optgroup label="Nouns">
+      <option value="Nb">Common nouns</option>
+      <option value="Ne">Proper nouns</option>
+    </optgroup>
+
+    <option value="A-">Adjectives</option>
+
+    <optgroup label="Adverbs">
+      <option value="Df">Adverbs</option>
+      <option value="Dq">Relative adverbs</option>
+      <option value="Du">Interrogative adverbs</option>
+    </optgroup>
+
+    <optgroup label="Pronouns">
+      <option value="Pc">Reciprocal pronouns</option>
+      <option value="Pd">Demonstrative pronouns</option>
+      <option value="Pi">Interrogative pronouns</option>
+      <option value="Pk">Personal reflexive pronouns</option>
+      <option value="Pp">Personal pronouns</option>
+      <option value="Pr">Relative pronouns</option>
+      <option value="Ps">Possessive pronouns</option>
+      <option value="Pt">Possessive reflexive pronouns</option>
+      <option value="Px">Indefinite pronouns</option>
+      <option value="Py">Quantifiers</option>
+    </optgroup>
+
+    <optgroup label="Numerals">
+      <option value="Ma">Cardinal numerals</option>
+      <option value="Mo">Ordinal numerals</option>
+    </optgroup>
+
+    <option value="S-">Articles</option>
+    <option value="R-">Prepositions</option>
+    <option value="N-">Infinitive markers</option>
+    <option value="C-">Conjunctions</option>
+    <option value="G-">Subjunctions</option>
+    <option value="I-">Interjections</option>
+
+    <option value="F-">Foreign words</option>
+
+    <option value="X-">Unassigned words</option>
+  </b-select>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: [String],
+      default: null,
+    }
+  },
+
+  data() {
+    return {
+      selected: this.value,
+    }
+  },
+
+  computed: {
+    computedValue: {
+      get() {
+        return this.selected
+      },
+
+      set(value) {
+        this.selected = value
+        this.$emit('input', value)
+      }
+    },
+  }
+}
+</script>
